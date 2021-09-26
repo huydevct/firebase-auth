073e83cc611f4d24ef6f8186814ad3b5a7fef5e5
huydevct <huyvipct1@gmail.com>
2021-09-27 00:02:08 +0700

    update

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
